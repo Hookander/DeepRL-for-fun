49a82d7d6a6ce15884694179b28277bb6c7e973f
Hookander <balthazar.martin123@gmail.com>
2024-07-17 18:49:05 +0200

    PPO !!

--- trainers/__init__.py
+++ trainers/__init__.py
@@ -2,8 +2,10 @@ from typing import Dict, Type
 from trainers.base_trainer import BaseTrainer
 from trainers.basic import BasicTrainer
 from trainers.parallelized_dqn import Parallelized_DQN
+from trainers.ppo import PPO
 
 trainer_name_to_TrainerClass : Dict[str, Type[BaseTrainer]] = {
     "basic" : BasicTrainer,
-    "parallel" : Parallelized_DQN
+    "parallel" : Parallelized_DQN,
+    "ppo" : PPO
 }
